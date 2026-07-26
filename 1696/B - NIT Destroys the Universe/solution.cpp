07a8995383009dd7e5ddea859d6cd7badb1b6616
files0792-ship-it <files0792@gmail.com>
2026-07-26 11:24:30 +0530

    Add NIT Destroys the Universe [B] from Codeforces

---
+++ 1696/B - NIT Destroys the Universe/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+int main(){
+     ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+ 
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)cin>>a[i];
+        
+        
+       int cnt=0;
+for(int i=0;i<n;i++){
+    if(a[i]!=0 && (i==0 || a[i-1]==0))
+        cnt++;
+}
+cout<<min(cnt,2)<<'
+';
+    }
+ 
+}
