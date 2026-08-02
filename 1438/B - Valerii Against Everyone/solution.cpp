5969dd74c56e0cd3dc8a9063886356f2d53091ac
files0792-ship-it <files0792@gmail.com>
2026-08-02 17:34:23 +0530

    Add Valerii Against Everyone [B] from Codeforces

---
+++ 1438/B - Valerii Against Everyone/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll unsigned long long
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<ll> vec;
+        for (int i = 0; i < n; i++)
+        {   
+            ll x;
+            cin>>x;
+            vec.push_back(x);
+        }
+        sort(vec.begin(),vec.end());
+        bool flag = true;
+        for (int i = 0; i < n-1; i++)
+        {
+            if(vec[i] == vec[i+1]) {
+                flag = false;
+                break;
+            }
+            
+        }
+        if(!flag)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;        
+        
+    }
+}
