573d899bfcd02d05b8b7810cbb45ea5e1d6c3a87
files0792-ship-it <files0792@gmail.com>
2026-08-02 13:38:26 +0530

    Add Ski Resort [C] from Codeforces

--- 1840/C - Ski Resort/solution.cpp
+++ 1840/C - Ski Resort/solution.cpp
@@ -1,43 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
 #define ll long long
- 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
- 
+int main()
+{
     int t;
     cin >> t;
- 
-    while (t--) {
-        ll n, k, q;
+    while (t--)
+    {
+        int n, k, q;
         cin >> n >> k >> q;
- 
-        ll ans = 0;
-        ll cnt = 0;
- 
-        for (int i = 0; i < n; i++) {
-            ll x;
-            cin >> x;
- 
-            if (x <= q) {
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        ll cnt = 0, ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] <= q)
+            {
                 cnt++;
-            } else {
-                if (cnt >= k) {
-                    ll x = cnt - k + 1;
-                    ans += x * (x + 1) / 2;
+            }
+            else
+            {
+                if (cnt >= k)
+                {
+                    ans += (cnt - k + 1) * (cnt - k + 2) / 2;
                 }
                 cnt = 0;
             }
         }
- 
-        if (cnt >= k) {
-            ll x = cnt - k + 1;
-            ans += x * (x + 1) / 2;
-        }
- 
-        cout << ans << "
-";
+        if (cnt >= k)
+            ans += (cnt - k + 1) * (cnt - k + 2) / 2;
+        cout << ans << endl;
     }
 }
