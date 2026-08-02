e6c530cd48e3922e49ba1a70923d66032e0b880c
files0792-ship-it <files0792@gmail.com>
2026-08-02 13:47:16 +0530

    Add Ski Resort [C] from Codeforces

--- 1840/C - Ski Resort/solution.cpp
+++ 1840/C - Ski Resort/solution.cpp
@@ -10,19 +10,15 @@ int main()
         int n, k, q;
         cin >> n >> k >> q;
         vector<int> v(n);
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
             cin >> v[i];
         }
         ll cnt = 0, ans = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (v[i] <= q)
-            {
+        for (int i = 0; i < n; i++){
+            if (v[i] <= q){
                 cnt++;
             }
-            else
-            {
+            else{
                 if (cnt >= k)
                 {
                     ans += (cnt - k + 1) * (cnt - k + 2) / 2;
