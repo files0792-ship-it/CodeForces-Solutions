6a0be82aa084568a8d5f470dd51a173db9f287fe
files0792-ship-it <files0792@gmail.com>
2026-08-27 12:12:18 +0530

    Add AB Balance [A] from Codeforces

---
+++ 1606/A - AB Balance/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;cin >> t;
+    while (t--)
+    {
+        string s;cin>>s;
+        s[s.size()-1] = s[0];
+        cout<<s<<endl;
+    }
+     
+    }
